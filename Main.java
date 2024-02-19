747f327a6da9bd256a05351079bf295194fb6399
V PRAMOD REDDY <160160713+vpramodreddy@users.noreply.github.com>
2024-02-19 22:58:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+ public static void main(String[] args)
+ {
+     int a;
+     float f;
+     char c;
+     String s;
+     a = 100;
+     f = 20.8f;
+     c ='P';
+     s = "PRAMOD REDDY";
+     System.out.println("Value of a:"+a);
+     System.out.println("Value of f:"+f);
+     System.out.println("Value of c:"+c);
+     System.out.println("Value of s:"+s);
+
+ }
+}
